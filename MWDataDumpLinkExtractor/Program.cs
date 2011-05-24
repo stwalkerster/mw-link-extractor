63cff27b30e3f5bddf0edb46b5a7fcf22ae05d08
Simon Walker <github@helpmebot.org.uk>
2011-05-25 00:58:22 +0100

    basic stuff

--- MWDataDumpLinkExtractor/Program.cs
+++ MWDataDumpLinkExtractor/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using MySql.Data;
 namespace MWDataDumpLinkExtractor
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // connect to database
+
+            // loop
+            {
+                // retrieve 1,000-10,000 rows
+
+                // loop
+                {
+                    // perform web check
+
+                    // update database
+                }
+            }
         }
     }
 }
