2a81afb7c24603aa7701de55ec5f4ec0c1bc7dc0
Simon Walker <github@helpmebot.org.uk>
2011-05-24 22:36:56 +0100

    del old code, new approach needed :D

--- MWDataDumpLinkExtractor/Program.cs
+++ MWDataDumpLinkExtractor/Program.cs
@@ -1,35 +1,10 @@
 ﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Xml.XPath;
 namespace MWDataDumpLinkExtractor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Stream infile;
-            StreamWriter outfile;
-
-
-            switch (args.Length)
-            {
-                case 0:
-                    infile = Console.OpenStandardInput();
-                    outfile = new StreamWriter("urls.out");
-                    break;
-                case 1:
-
-                    throw new ArgumentException();
-                default:
-                    infile = new FileStream(args[0], FileMode.Open);
-                    outfile = new StreamWriter(args[1]);
-                    break;
-            }
-
-
-            StreamReader infilereader = new StreamReader(infile);
-            infilereader.
         }
     }
 }
