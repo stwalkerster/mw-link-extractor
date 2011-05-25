722fd8a087dd5b2e99ee6884a0e740610848081e
Simon Walker <github@helpmebot.org.uk>
2011-05-25 17:05:41 +0100

    add an event args class which accepts a token as the data.

---
+++ MWDataDumpLinkExtractor/TokenEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MWDataDumpLinkExtractor
+{
+    class TokenEventArgs : EventArgs
+    {
+        public TokenEventArgs()
+        {
+            
+        }
+
+        public TokenEventArgs(string token)
+        {
+            this.token = token;    
+        }
+
+        public string token { get; set; }
+    }
+}
