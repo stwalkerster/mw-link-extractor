97cc8d79bacb91eccde36b66264509f2fcc3c025
Simon Walker <github@stwalkerster.co.uk>
2012-04-30 15:15:40 +0100

    update to new method

---
+++ MWDataDumpLinkExtractor/DotHmbotConfigurationFile.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace TableSplitter
+{
+    /// <summary>
+    /// Provides an interface for using the .hmbot configuration files
+    /// </summary>
+    public class DotHmbotConfigurationFile
+    {
+        /// <summary>
+        /// XML Root namespace/schema/something...
+        /// </summary>
+        public const string NS_ROOT = "";
+
+        /// <summary>
+        /// MySQL constants for use with the configuration. Provided for backwards compatiability
+        /// </summary>
+        public const string MYSQL_SERVER = "mySqlServerHostname",
+                            MYSQL_PORT = "mySqlServerPort",
+                            MYSQL_USERNAME = "mySqlUsername",
+                            MYSQL_PASSWORD = "mySqlPassword",
+                            MYSQL_SCHEMA = "mySqlSchema";
+
+        private string filename = "";
+        readonly Dictionary<string, string> _store = new Dictionary<string, string>();
+
+        private void readVer1ConfigFile()
+        {
+            StreamReader settingsreader = new StreamReader(filename);
+            this[MYSQL_SERVER] = settingsreader.ReadLine();
+            this[MYSQL_PORT] = settingsreader.ReadLine();
+            this[MYSQL_USERNAME] = settingsreader.ReadLine();
+            this[MYSQL_PASSWORD] = settingsreader.ReadLine();
+            this[MYSQL_SCHEMA] = settingsreader.ReadLine();
+            settingsreader.Close();
+
+        }
+
+        private DotHmbotConfigurationFile()
+        {
+        }
+
+
+
+        ///<summary>
+        /// Setting store
+        ///</summary>
+        ///<param name="key">Setting name</param>
+        public string this[string key]
+        {
+            get
+            {
+                return _store[key];
+            }
+            set
+            {
+                _store[key] = value;
+            }
+        }
+
+        public static DotHmbotConfigurationFile Create(string filename, string serverHostname, uint serverPort,
+                                        string username, string password, string schema)
+        {
+            DotHmbotConfigurationFile x = new DotHmbotConfigurationFile { filename = filename };
+            x[MYSQL_SERVER] = serverHostname;
+            x[MYSQL_PORT] = serverPort.ToString();
+            x[MYSQL_USERNAME] = username;
+            x[MYSQL_PASSWORD] = password;
+            x[MYSQL_SCHEMA] = schema;
+
+            x.save();
+
+            return x;
+        }
+
+        public static DotHmbotConfigurationFile Create(string filename)
+        {
+            DotHmbotConfigurationFile x = new DotHmbotConfigurationFile { filename = filename };
+
+            x.save();
+
+            return x;
+        }
+
+        public static DotHmbotConfigurationFile Open(string filename)
+        {
+            DotHmbotConfigurationFile x = new DotHmbotConfigurationFile { filename = filename };
+
+
+            StreamReader sr = new StreamReader(filename);
+            string lineone = sr.ReadLine();
+            sr.Close();
+
+            if (lineone.Contains("<?xml"))
+            {
+                x.readXmlConfigFile();
+            }
+            else
+            {
+                x.readVer1ConfigFile();
+            }
+
+            return x;
+        }
+
+        private void readXmlConfigFile()
+        {
+            XPathDocument xpd = new XPathDocument(filename);
+            XPathNavigator xpn = xpd.CreateNavigator();
+            XPathNodeIterator xpni = xpn.Select("/hmbot[@version='2']/config/option");
+            while (xpni.MoveNext())
+            {
+                _store[xpni.Current.GetAttribute("name", "")] = xpni.Current.Value;
+            }
+        }
+
+        public void save()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.AppendChild(doc.CreateXmlDeclaration("1.0", null, null));
+            XmlElement hmbot = doc.CreateElement("hmbot", NS_ROOT);
+            XmlElement config = doc.CreateElement("config");
+            hmbot.SetAttribute("version", "2");
+            hmbot.AppendChild(config);
+            doc.AppendChild(hmbot);
+
+            foreach (KeyValuePair<string, string> item in _store)
+            {
+                XmlElement option = doc.CreateElement("option");
+                option.SetAttribute("name", item.Key);
+                option.AppendChild(doc.CreateTextNode(item.Value));
+                config.AppendChild(option);
+            }
+
+            XmlTextWriter xtw = new XmlTextWriter(filename, Encoding.ASCII) { Formatting = Formatting.Indented };
+            doc.WriteTo(xtw);
+            xtw.Flush();
+            xtw.Close();
+        }
+    }
+}
--- MWDataDumpLinkExtractor/Program.cs
+++ MWDataDumpLinkExtractor/Program.cs
@@ -1,44 +1,105 @@
 ﻿using System;
 using System.IO;
-using MySql.Data;
-using Utility;
+using MySql.Data.MySqlClient;
 
-namespace MWDataDumpLinkExtractor
+namespace TableSplitter
 {
     class Program
     {
-        private static DatabaseLayer dal;
-
         static void Main(string[] args)
         {
-            if (!new FileInfo("dbconfig.txt").Exists)
+            if (!new FileInfo("dbconfig.xml").Exists)
             {
-                DotHmbotConfigurationFile.Create("dbconfig.txt", "host", 3306, "username", "password", "schema");
+                DotHmbotConfigurationFile.Create("dbconfig.xml", "host", 3306, "username", "password", "schema");
 
-                return;   
+                return;
             }
 
-            DotHmbotConfigurationFile dotHmbotConfigurationFile = new DotHmbotConfigurationFile("dbconfig.txt");
+            DotHmbotConfigurationFile dotHmbotConfigurationFile = DotHmbotConfigurationFile.Open("dbconfig.xml");
 
-            dal = new DatabaseLayer(
-                dotHmbotConfigurationFile.mySqlServerHostname,
-                dotHmbotConfigurationFile.mySqlServerPort,
-                dotHmbotConfigurationFile.mySqlUsername,
-                dotHmbotConfigurationFile.mySqlPassword,
-                dotHmbotConfigurationFile.mySqlSchema,
-                1000
-                );
+            MySqlConnection connection = new MySqlConnection(
+                new MySqlConnectionStringBuilder()
+                {
+                    UserID = dotHmbotConfigurationFile[DotHmbotConfigurationFile.MYSQL_USERNAME],
+                    Password = dotHmbotConfigurationFile[DotHmbotConfigurationFile.MYSQL_PASSWORD],
+                    Server = dotHmbotConfigurationFile[DotHmbotConfigurationFile.MYSQL_SERVER],
+                    Database = dotHmbotConfigurationFile[DotHmbotConfigurationFile.MYSQL_SCHEMA]
+                }.ToString());
 
-            dal.NextSetReadyEvent += firstSetReadyEventHandler;
-
-            dal.initialise();
-        }
-
-        static void firstSetReadyEventHandler(object sender, TokenEventArgs e)
-        {
-            dal.NextSetReadyEvent -= firstSetReadyEventHandler;
+            connection.Open();
 
 
+            while(true)
+            {
+                // start transaction
+                MySqlTransaction t = connection.BeginTransaction(System.Data.IsolationLevel.RepeatableRead);
+
+                // select 1 (or more) from externallinks FOR UPDATE
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM externallinks LIMIT 1 FOR UPDATE;", connection, t);
+                MySqlDataReader r = cmd.ExecuteReader();
+
+                if (!r.Read()) break;
+
+                int page = r.GetInt32(0);
+                string url = r.GetString(1);
+                string idx = r.GetString(2);
+
+                r.Close();
+
+                // insert into processed
+                cmd = new MySqlCommand("INSERT INTO processed VALUES( @1, @2, @3);", connection, t);
+                cmd.Parameters.AddWithValue("@1", page);
+                cmd.Parameters.AddWithValue("@2", url);
+                cmd.Parameters.AddWithValue("@3", idx);
+                cmd.ExecuteNonQuery();
+
+                // delete it from externallinks
+                cmd = new MySqlCommand("DELETE FROM externallinks WHERE el_from = @1 AND el_to = @2 AND el_index = @3;", connection, t);
+                cmd.Parameters.AddWithValue("@1", page);
+                cmd.Parameters.AddWithValue("@2", url);
+                cmd.Parameters.AddWithValue("@3", idx);
+                //cmd.ExecuteNonQuery();
+
+                Console.WriteLine(url);
+
+                // (split data)
+                Uri u = new Uri(url);
+                string protocol = u.Scheme,
+                    domain = u.DnsSafeHost;
+
+                // insert ignore into protocol
+                cmd = new MySqlCommand("INSERT IGNORE INTO protocol (protocol) VALUES (@1) ;", connection, t);
+                cmd.Parameters.AddWithValue("@1", protocol);
+                cmd.ExecuteNonQuery();
+
+                // get protocol id
+                cmd = new MySqlCommand("SELECT id FROM protocol WHERE protocol = @1 ;", connection, t);
+                cmd.Parameters.AddWithValue("@1", protocol);
+                int protocolid = (int)cmd.ExecuteScalar();
+
+                // insert ignore into domain
+                cmd = new MySqlCommand("INSERT IGNORE INTO domain (domain) VALUES (@1) ;", connection, t);
+                cmd.Parameters.AddWithValue("@1", domain);
+                cmd.ExecuteNonQuery();
+
+                // get domain id
+                cmd = new MySqlCommand("SELECT id FROM domain WHERE domain = @1 ;", connection, t);
+                cmd.Parameters.AddWithValue("@1", domain);
+                int domainid = (int)cmd.ExecuteScalar();
+
+                // insert into link protocol domain link fragment page
+                cmd = new MySqlCommand("INSERT INTO link (protocol, domain, path, fragment, page) VALUES( @1, @2, @3, @4, @5);", connection, t);
+                cmd.Parameters.AddWithValue("@5", page);
+                cmd.Parameters.AddWithValue("@2", domainid);
+                cmd.Parameters.AddWithValue("@1", protocolid);
+                cmd.Parameters.AddWithValue("@3", u.PathAndQuery);
+                cmd.Parameters.AddWithValue("@4", u.Fragment);
+                cmd.ExecuteNonQuery();
+
+                // commit
+                t.Commit();
+                
+            }
         }
     }
 }
